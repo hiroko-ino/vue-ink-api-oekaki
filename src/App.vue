b8122b80b7b11e9d942f9ff0dfd7526ff1d4aead
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-09-23 17:11:01 +0900

    feat: ✨ ink api base

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+
+declare var navigator:any;
+
+const canvasRef = ref<HTMLCanvasElement | null>(null)
+const style = ref({ color: "rgba(0, 255, 0, 1)", diameter: 10 })
+
+onMounted(async () => {
+  if (!canvasRef.value) return
+  const ctx = canvasRef.value.getContext("2d")
+  const presenter = await navigator.ink.requestPresenter({ presentationArea: canvasRef.value });
+
+  console.log(presenter)
+
+  canvasRef.value.addEventListener("pointermove", async (evt) => {
+    if (ctx === null) return
+    const pointSize = 10;
+    ctx.fillStyle = style.value.color;
+    ctx.fillRect(evt.pageX, evt.pageY, pointSize, pointSize);
+    await presenter.updateInkTrailStartPoint(evt, style.value);
+  })
+
+  window.addEventListener("pointerdown", () => {
+    if (ctx === null) return
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  })
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <canvas width="800" height="500" id="canvas" ref="canvasRef"></canvas>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
